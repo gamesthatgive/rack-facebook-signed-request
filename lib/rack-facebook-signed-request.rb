f027d13371cd9fc3dcca8ef8ffd61e950edcd397
= <=>
2010-11-18 15:56:15 -0500

    first commit

---
+++ lib/rack-facebook-signed-request.rb
@@ -0,0 +1,1 @@
+require 'rack/facebook/signed_request'
---
+++ lib/rack/facebook/signed_request.rb
@@ -0,0 +1,56 @@
+require 'openssl'
+require 'base64'
+require 'yajl'
+
+module Rack
+  module Facebook
+    class SignedRequest
+      def initialize(app, options, &condition)
+        @app = app
+        @condition = condition
+        @options = options
+      end
+
+      def secret
+        @options.fetch(:secret)
+      end
+
+      def call(env)
+        request = Rack::Request.new(env)
+
+        signed_request = request.params.delete('signed_request')
+        unless signed_request
+          return Rack::Response.new(["Missing signed_request param"], 400).finish
+        end
+
+        signature, signed_params = signed_request.split('.')
+
+        unless signed_request_is_valid?(secret, signature, signed_params)
+          return Rack::Response.new(["Invalid signature"], 400).finish
+        end
+
+        signed_params = Yajl::Parser.new.parse(base64_url_decode(signed_params))
+
+        # add JSON params to request
+        signed_params.each do |k,v|
+          request.params[k] = v
+        end
+
+        @app.call(env)
+      end
+
+      private
+
+        def signed_request_is_valid?(secret, signature, params)
+          signature = base64_url_decode(signature)
+          expected_signature = OpenSSL::HMAC.digest('SHA256', secret, params.tr("-_", "+/"))
+          return signature == expected_signature
+        end
+
+        def base64_url_decode(str)
+          str = str + "=" * (6 - str.size % 6) unless str.size % 6 == 0
+          return Base64.decode64(str.tr("-_", "+/"))
+        end
+    end
+  end
+end
